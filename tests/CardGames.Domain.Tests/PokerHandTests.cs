4880258ac63523702dfb0c176b4fe89984fc2b73
Rafael <Rafael@DESKTOP-G1V9THK>
2023-08-06 14:40:42 -0300

    Adicionar arquivos de projeto.

---
+++ tests/CardGames.Domain.Tests/PokerHandTests.cs
@@ -0,0 +1,25 @@
+namespace CardGames.Domain.Tests
+{
+    public class PokerHandTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        public interface IHand
+        {
+
+        }
+
+        public abstract class Hand : IHand { 
+        
+        
+        }
+
+        public class PokerHand : Hand{
+
+        }
+    }
+}
